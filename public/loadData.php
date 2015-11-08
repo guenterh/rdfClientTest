c4c0342deb755c13d741df13acc3af5d18437dec
Guenter Hipler <guenter.hipler@unibas.ch>
2015-11-08 10:13:11 +0100

    rdfxml example included

--- public/loadData.php
+++ public/loadData.php
@@ -7,14 +7,18 @@ require "../vendor/autoload.php";
 
 //$data = file_get_contents("../data/sw.p26_1.ttl");
 //$data = file_get_contents("../data/sw.p26_1_1.json");
-$data = file_get_contents("../data/sw.p27.ttl");
+//$data = file_get_contents("../data/sw.p27.ttl");
+$data = file_get_contents("../data/sw.p26.xml");
+
+
 
 $graph = new EasyRdf_Graph();
-$numbertriples = $graph->parse($data,'turtle');
+EasyRdf_Namespace::set("ex", "http://example.org/");
+$numbertriples = $graph->parse($data,'rdfxml');
 //$numbertriples = $graph->parse($data,'jsonld');
 
 
 
 //$graph->load();
-$json = $graph->serialise("jsonld");
+$json = $graph->serialise("turtle");
 echo $json;
