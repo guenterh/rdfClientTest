5a6c444117cd4158c089d5128164ea4ca571e6ee
Guenter Hipler <guenter.hipler@unibas.ch>
2015-11-08 14:45:26 +0100

    new examples

--- public/loadData.php
+++ public/loadData.php
@@ -3,22 +3,30 @@
 
 require "../vendor/autoload.php";
 
+
+//serialization constants:
+//rdfxml, ntriples, jsonld, turtle
+
 //$data = include("../data/turtle.ttl");
 
 //$data = file_get_contents("../data/sw.p26_1.ttl");
 //$data = file_get_contents("../data/sw.p26_1_1.json");
 //$data = file_get_contents("../data/sw.p27.ttl");
-$data = file_get_contents("../data/sw.p26.xml");
+$data = file_get_contents("../data/hpi2015/week1/lec8/p11/dereferencable.blank.nodes.ttl");
 
 
 
 $graph = new EasyRdf_Graph();
-EasyRdf_Namespace::set("ex", "http://example.org/");
-$numbertriples = $graph->parse($data,'rdfxml');
+EasyRdf_Namespace::set("dbo", "http://dbpedia.org/ontology/");
+EasyRdf_Namespace::set("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+EasyRdf_Namespace::set("rdfs", "http://www.w3.org/2000/01rdf-schema#");
+
+$numbertriples = $graph->parse($data,'turtle');
+
 //$numbertriples = $graph->parse($data,'jsonld');
 
 
 
 //$graph->load();
-$json = $graph->serialise("turtle");
+$json = $graph->serialise("rdfxml");
 echo $json;
