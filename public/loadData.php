61a90299b4618e3bd9e4767204b2c1218cebada4
Guenter Hipler <guenter.hipler@unibas.ch>
2015-11-16 23:03:53 +0100

    new example data for models

--- public/loadData.php
+++ public/loadData.php
@@ -12,7 +12,7 @@ require "../vendor/autoload.php";
 //$data = file_get_contents("../data/sw.p26_1.ttl");
 //$data = file_get_contents("../data/sw.p26_1_1.json");
 //$data = file_get_contents("../data/sw.p27.ttl");
-$data = file_get_contents("../data/hpi2015/week1/lec8/p16/short.collection.ttl");
+$data = file_get_contents("../data/hpi2015/week2/rdfs/p11/rdfsExample.ttl");
 
 
 
@@ -20,8 +20,8 @@ $graph = new EasyRdf_Graph();
 EasyRdf_Namespace::set("dbo", "http://dbpedia.org/ontology/");
 EasyRdf_Namespace::set("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
 EasyRdf_Namespace::set("rdfs", "http://www.w3.org/2000/01rdf-schema#");
-EasyRdf_Namespace::set("ex", "http://example.org/test#");
-
+EasyRdf_Namespace::set("ex", "http://example.org/Crimestories#");
+EasyRdf_Namespace::set("dbpedia", "http://dbpedia.org/resource/");
 
 $numbertriples = $graph->parse($data,'turtle');
 
@@ -30,5 +30,5 @@ $numbertriples = $graph->parse($data,'turtle');
 
 
 //$graph->load();
-$json = $graph->serialise("jsonld");
+$json = $graph->serialise("rdfxml");
 echo $json;
